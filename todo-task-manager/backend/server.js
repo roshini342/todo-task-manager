4182927bf4a6c2b12de239d42bace46a6f918314
roshini342 <165682446+roshini342@users.noreply.github.com>
2025-07-10 11:10:33 +0530

    Update server.js

--- todo-task-manager/backend/server.js
+++ todo-task-manager/backend/server.js
@@ -7,7 +7,7 @@ const Todo = require('./models/Todo');
 dotenv.config(); // Load .env variables
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
